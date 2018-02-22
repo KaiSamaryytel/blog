830018c8bfb96c7ba4e0eb8fc18c8055f0c9a645
KaiSamaryytel <kai.koppel@khk.ee>
2018-02-22 15:14:56 +0200

    Nyyd peaks olema kõik!

---
+++ controllers/tags.php
@@ -0,0 +1,18 @@
+<?php namespace App;
+
+class tags extends Controller
+{
+    function index(){
+        $this->tags = get_all(
+            "SELECT post_id, tag_name, ".
+            "COUNT(post_id) AS count ".
+            "FROM post_tags NATURAL JOIN tag ".
+            "GROUP BY tag_id");
+    }
+    function view(){
+        $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
+        foreach($_tags as $tag){
+            $this->tags[$tag['post_id']][] = $tag['tag_name'];
+        }
+    }
+}
